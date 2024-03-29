ebf2d1759c2325b18da2c4129fdb1c784f088b0c
Dawit Yonas Sahlu <dawityonas010@gmail.com>
2024-03-30 02:00:10 +0300

    Time: 183 ms (66.88%), Space: 17.8 MB (7.72%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,31 +1,16 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
+        visited = set()
         
-        uf = [i for i in range(n)]
-        siz = [1 for i in range(n)]
-        
-        def find(i):
-            if uf[i] == i:
-                return uf[i]
-            uf[i] = find(uf[i])
-            return uf[i]
-        
-        def union(i,j):
-            pi =find(i)
-            pj = find(j)
-            if siz[pi] > siz[pj]:
-                pi,pj = pj,pi
-            uf[pj] = uf[pi]
-            siz[pi] += siz[pj]
-            
-        def connected(i,j):
-            return find(i) == find(j)
-        
-        for i in range(n):
-            for j in range(n):
-                if isConnected[i][j]:
-                    union(i,j)
+        def dfs(i):
+            visited.add(i)
+            for j in range(len(isConnected)):
+                if j not in visited and isConnected[i][j]:
+                    dfs(j)
                     
-            
-        return len(set([find(i) for i in range(n)]))
+        provinces = 0    
+        for i in range(len(isConnected)):
+            if i not in visited:
+                provinces += 1
+                dfs(i)
+        return provinces
